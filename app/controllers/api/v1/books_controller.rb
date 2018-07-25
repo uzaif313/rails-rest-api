622282e7adae55c6a2ad6137773c02b413c7dda2
Uzaif Nilger <uzaif313@gmail.com>
2018-07-25 19:52:59 +0530

    implement book api controller

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::BooksController < ApplicationController
+  before_action :load_book, only: :show
+
+  def index
+    @books = Book.all
+    json_response "List Of Books",true, @books, :ok
+  end
+
+  def show
+    json_response "Detail of Book", true, @book, :ok
+  end
+
+  private
+    def load_book
+      @book = Book.find_by(id:params[:id])
+      unless @book.present?
+        json_response "Book Not found", false, {}, :not_found
+      end
+    end
+end
