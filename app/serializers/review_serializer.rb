b2a67b3f3248e9378ca63209c1fa3bf114f770a1
Prakash Chaudhary <chaudharyprakash1992@gmail.com>
2018-09-14 13:33:28 +0530

    display image url on review response

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,10 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content_rating, :recommend_rating, :average_rating
+  include Rails.application.routes.url_helpers
+  attributes :id, :title, :content_rating, :recommend_rating, :average_rating, :review_image
   belongs_to :user
   belongs_to :book
+
+  def review_image
+    rails_blob_path(object.image) if object.image.attachment
+  end
 end
